cd3bd5f5ada39c85c5ffe0296afda84179483851
Martin <cph-ms782@cphbusiness.dk>
2020-05-21 14:46:52 +0200

    rydder op i foldere og filer

--- testing/json_test.py
+++
@@ -1,6 +0,0 @@
-import json
-
-with open("./testing/KomkodeAntal_linjer.json", "r") as read_file:
-    data = json.load(read_file)
-    print(data)
-    print(data['0101'])
--- testing/tabulaTest.py
+++
@@ -1,4 +0,0 @@
-import tabula
-
-# convert PDF into CSV
-tabula.convert_into("test.pdf", "output.csv", output_format="csv", pages='all')
